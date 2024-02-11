4b09333e12c6cafec2a20fc787c2c9541b4e3d18
VaibhavManav <2018kucp1018@iiitkota.ac.in>
2024-02-11 15:51:33 +0530

    commit

--- demo.cpp
+++ demo.cpp
@@ -4,17 +4,7 @@
 using namespace std;
 
 int main() {
-    vector<int> nums =  {1,1,0,1,1,1,0,1};
-	 int m = 0,c = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == 0){
-                m = max(m,c);
-                c = 0;
-            }
-            else
-            c = c+1;
-
-            m = max(m,c);
-        }
-        cout<<c;
+    char c = 'b';
+    string e = to_string(c-'a');
+    cout<<e;
 }
